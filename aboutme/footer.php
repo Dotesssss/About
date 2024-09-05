8473a740289a6454c8ec2d6f31a6a39325143ff3
Dotesssss <jgarabit2812ant@student.fatima.edu.ph>
2024-09-05 19:38:53 +0800

    Delete aboutme directory

--- aboutme/footer.php
+++
@@ -1,7 +0,0 @@
-</main>
-    <footer>
-        <p>Check out my Social Media Accounts</p>
-        <p><a href="https://www.facebook.com/Joshhh.arabit" target="_blank">Facebook</a> and <a href="https://www.instagram.com/dotes999/" target="_blank">Instagram</a></p>
-    </footer>
-</body>
-</html>
